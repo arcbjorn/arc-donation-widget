ae39b2d4b58d2d2ff96a2e880499d2f16d68ffa3
arcbjorn <arky.happ@gmail.com>
2021-04-29 23:08:25 +0300

    feat (client): add default types

---
+++ client/src/types/index.ts
@@ -0,0 +1,20 @@
+export enum currencyName {
+  USD = "US Dollar",
+  EUR = "Euro",
+  GBP = "British Pound",
+  RUB = "Russian Rubble",
+}
+
+export enum currencyCode {
+  USD = "USD",
+  EUR = "EUR",
+  GBP = "GBP",
+  RUB = "RUB",
+}
+
+export enum currencySymbol {
+  USD = "$",
+  EUR = "€",
+  GBP = "£",
+  RUB = "₽",
+}
