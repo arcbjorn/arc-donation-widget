3ff7048686983db8f93987c586e94453524c73a4
arcbjorn <arky.happ@gmail.com>
2021-04-29 23:15:24 +0300

    refactor (server): update entrypoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,18 +4,20 @@ import logger from 'koa-logger';
 import json from 'koa-json';
 import bodyParser from 'koa-bodyparser';
 import router from './router';
-import connect from './database/connect';
+import { connect } from './database';
 
 const app = new Koa();
 
-// 'mongodb://widget:secret@mongodb:27017/widgetdb/?authSource=admin'
+// 'mongodb://widget:secret@mongodb:27017/widgetdb?authSource=admin'
 const DB = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 8081;
 
 // Middleware
+// app.use(cors());
 app.use(json());
 app.use(logger());
 app.use(bodyParser());
+
 // Routes
 app.use(router.routes()).use(router.allowedMethods());
 
