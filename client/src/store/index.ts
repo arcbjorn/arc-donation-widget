fdd8985084aaf3ccdb093afe72c1178ebc908124
arcbjorn <arky.happ@gmail.com>
2021-05-01 20:30:03 +0300

    feat (client/store): add state, setDonationValue

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,16 +1,37 @@
 import { InjectionKey } from "vue";
 import { createStore, useStore as baseUseStore, Store } from "vuex";
+import { Currency, CurrencyCode, MutationType, Preset } from "@/types";
+import { currencies, initPresets } from "./initStore";
 
 export interface State {
-  count: number;
+  currencies: Currency[];
+  presets: Preset[];
+  donationValue: number;
+  activeCurrency: Currency;
 }
 
 export const key: InjectionKey<Store<State>> = Symbol();
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    currencies,
+    presets: initPresets(),
+    donationValue: 40,
+    activeCurrency: currencies[0],
+  },
+  mutations: {
+    setDonationValue(state, value: number): void {
+      state.donationValue = value;
+    },
+  },
+  actions: {
+    setDonationValue({ commit, state }, preset: Preset): void {
+      commit(
+        MutationType.setDonationValue,
+        preset[state.activeCurrency.code as CurrencyCode]
+      );
+    },
+  },
   modules: {},
 });
 
