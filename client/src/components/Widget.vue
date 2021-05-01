ddf8f5b231adfd45c303839e454982c4a6f343d9
arcbjorn <arky.happ@gmail.com>
2021-05-01 20:05:26 +0300

    feat (client/src): add Widget component

---
+++ client/src/components/Widget.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+h1 {{ title }}
+.widget
+  SuggestionsButtonGroup
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SuggestionsButtonGroup from "@/components/SuggestionsButtonGroup.vue";
+
+export default defineComponent({
+  name: "Widget",
+  props: {
+    title: String,
+  },
+  components: {
+    SuggestionsButtonGroup,
+  },
+});
+</script>
+
+<style scoped></style>
