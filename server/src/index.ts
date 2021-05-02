44f881d3d3fc09ffcd364e194238a7f020ff0036
arcbjorn <arky.happ@gmail.com>
2021-05-03 02:00:32 +0300

    chore (server): add CORS handling & dependency

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import Koa from 'koa';
 import logger from 'koa-logger';
 import json from 'koa-json';
 import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
+
 import router from './router';
 import { connect } from './database';
 import { ConsoleMessageType } from './types';
@@ -14,7 +16,7 @@ const DB = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 8081;
 
 // Middleware
-// app.use(cors());
+app.use(cors());
 app.use(json());
 app.use(logger());
 app.use(bodyParser());
