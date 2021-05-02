4b1daf79ea4680624f9d241b8eddb922a8ed9300
arcbjorn <arky.happ@gmail.com>
2021-05-02 23:00:55 +0300

    feat (client/components): add CurrencySelect

---
+++ client/src/components/CurrencySelect.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+select(:value="activeCurrency.code" @change="setActiveCurrency($event.target.value)")
+  option(
+    v-for="currency, i in currencies"
+    key="i"
+    :value="currency.code"
+  ) {{ currency.code }}
+</template>
+
+<script lang="ts">
+import { ActionType, GetterType, StateType } from "@/types";
+import { defineComponent } from "vue";
+import { mapActions, mapGetters, mapState } from "vuex";
+
+export default defineComponent({
+  name: "CurrencySelect",
+  computed: {
+    ...mapGetters([GetterType.currencies]),
+    ...mapState([StateType.activeCurrency]),
+  },
+  methods: {
+    ...mapActions([ActionType.setActiveCurrency]),
+  },
+});
+</script>
+
+<style scoped></style>
