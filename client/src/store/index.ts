e1cf18776bb876a47818398c17528849c510139b
arcbjorn <arky.happ@gmail.com>
2021-05-02 23:00:12 +0300

    feat(client/store): add setActiveCurrency &updates

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,10 +1,16 @@
 import { InjectionKey } from "vue";
 import { createStore, useStore as baseUseStore, Store } from "vuex";
-import { Currency, CurrencyCode, MutationType, Preset } from "@/types";
-import { currencies, initPresets } from "./initStore";
+import {
+  Currency,
+  CurrencyStore,
+  CurrencyCode,
+  MutationType,
+  Preset,
+} from "@/types";
+import { initCurrencyStore, initPresets } from "./initStore";
 
 export interface State {
-  currencies: Currency[];
+  currencyStore: CurrencyStore;
   presets: Preset[];
   donationValue: number;
   activeCurrency: Currency;
@@ -12,16 +18,27 @@ export interface State {
 
 export const key: InjectionKey<Store<State>> = Symbol();
 
+const store = initCurrencyStore();
+const presetsArray = initPresets();
+
 export default createStore({
   state: {
-    currencies,
-    presets: initPresets(),
+    currencyStore: store,
+    presets: presetsArray,
     donationValue: 40,
-    activeCurrency: currencies[0],
+    activeCurrency: store[CurrencyCode.USD],
+  },
+  getters: {
+    currencies(state): Currency[] {
+      return Object.values(state.currencyStore);
+    },
   },
   mutations: {
     setDonationValue(state, value: number): void {
       state.donationValue = value;
+    },
+    setActiveCurrency(state, value: string): void {
+      state.activeCurrency = state.currencyStore[value];
     },
   },
   actions: {
@@ -31,6 +48,9 @@ export default createStore({
           ? value
           : value[state.activeCurrency.code as CurrencyCode];
       commit(MutationType.setDonationValue, val);
+    },
+    setActiveCurrency({ commit }, code: string): void {
+      commit(MutationType.setActiveCurrency, code);
     },
   },
   modules: {},
